62e675491ab454a985186d35029bf337bc9d0cf2
PrantoshB <pbiswas1007@gmail.com>
2022-11-14 23:32:59 +0530

    Mobile Menu

---
+++ script.js
@@ -0,0 +1,8 @@
+function OpenMenu(){
+    document.querySelector('.menuformobile').style.display = "flex"
+    document.querySelector('.closeicon').style.display = "block"
+}
+function CloseMenu(){
+    document.querySelector('.menuformobile').style.display = "none"
+    document.querySelector('.closeicon').style.display = "none"
+}
