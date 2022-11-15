de4273e9f29ed346c7a04270b0e358189e5265f2
Salwa99 <s.ritage99@gmail.com>
2022-11-15 11:10:28 +0100

    update css and js file

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function OpenMenu(){
     document.querySelector('.menuformobile').style.display = "flex"
     document.querySelector('.closeicon').style.display = "block"
+    document.querySelector('.openicon').style.display = "none"
+    document.querySelector('.myLogo').style.display = "none"
+    document.querySelector('.info').style.display = "none"
+
 }
 function CloseMenu(){
     document.querySelector('.menuformobile').style.display = "none"
     document.querySelector('.closeicon').style.display = "none"
+    document.querySelector('.openicon').style.display = "block"
+    document.querySelector('.myLogo').style.display = "flex"
+    document.querySelector('.info').style.display = "flex"
 }
