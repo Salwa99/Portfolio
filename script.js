01d26b0e75286a9754fa9b695839411ea23ad2a0
PrantoshB <pbiswas1007@gmail.com>
2022-11-15 17:30:40 +0530

    Fixed the menu

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+const linkPortfolio = document.querySelector('#portfolio-link')
+const linkAbout = document.querySelector('#about-link')
+const linkContact = document.querySelector('#contact-link')
+
 function OpenMenu(){
     document.querySelector('.menuformobile').style.display = "flex"
     document.querySelector('.closeicon').style.display = "block"
     document.querySelector('.openicon').style.display = "none"
     document.querySelector('.myLogo').style.display = "none"
-    document.querySelector('.info').style.display = "none"
+    document.querySelector('.info').style.filter = " blur(2px)"
+    document.querySelector('body').style.backgroundColor = "rgba(96, 112, 255, 1)"
+
 
 }
 function CloseMenu(){
@@ -11,5 +17,31 @@ function CloseMenu(){
     document.querySelector('.closeicon').style.display = "none"
     document.querySelector('.openicon').style.display = "block"
     document.querySelector('.myLogo').style.display = "flex"
-    document.querySelector('.info').style.display = "flex"
+    document.querySelector('.info').style.filter = " blur(0)"
+    document.querySelector('body').style.backgroundColor = "transparent"
+
 }
+linkPortfolio.addEventListener('click', () => {
+    document.querySelector('.menuformobile').style.display = "none"
+    document.querySelector('.closeicon').style.display = "none"
+    document.querySelector('.openicon').style.display = "block"
+    document.querySelector('.myLogo').style.display = "flex"
+    document.querySelector('.info').style.filter = " blur(0)"
+    document.querySelector('body').style.backgroundColor = "transparent"
+})
+linkAbout.addEventListener('click', () => {
+    document.querySelector('.menuformobile').style.display = "none"
+    document.querySelector('.closeicon').style.display = "none"
+    document.querySelector('.openicon').style.display = "block"
+    document.querySelector('.myLogo').style.display = "flex"
+    document.querySelector('.info').style.filter = " blur(0)"
+    document.querySelector('body').style.backgroundColor = "transparent"
+})
+linkContact.addEventListener('click', () => {
+    document.querySelector('.menuformobile').style.display = "none"
+    document.querySelector('.closeicon').style.display = "none"
+    document.querySelector('.openicon').style.display = "block"
+    document.querySelector('.myLogo').style.display = "flex"
+    document.querySelector('.info').style.filter = " blur(0)"
+    document.querySelector('body').style.backgroundColor = "transparent"
+})
