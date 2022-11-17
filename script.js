0e7c703dd23e2389b177d07126c237e82a912b5c
PrantoshB <Pbiswas1007@gmail.com>
2022-11-18 01:25:06 +0530

    Add Modal

--- script.js
+++ script.js
@@ -1,15 +1,166 @@
+
+const projectsList = [
+  {
+    mdltitle: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'img/Snapshoot Portfolio.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Back end dev',
+    company: 'canopy',
+    year: '2015',
+    sourceLink: '#',
+    demoLink: '#',
+  },
+  {
+    mdltitle: 'Multi-post stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'img/Snapshoot Portfolio2.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Full Stack Dev',
+    company: 'Facebook',
+    year: '2015',
+    sourceLink: '#',
+    demoLink: '#',
+  },
+  {
+    mdltitle: 'Facebook 360',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. ',
+    imageLink: 'img/Snapshoot Portfolio3.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Back end Dev',
+    company: 'Facebook',
+    year: '2015',
+    sourceLink: '#',
+    demoLink: '#',
+  },
+  {
+    mdltitle: 'Uber Navigation',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    imageLink: 'img/Snapshoot Portfolio4.png',
+    technologies: ['html', 'css', 'javascript'],
+    role: 'Lead developer',
+    company: 'Uber',
+    year: '2018',
+    sourceLink: '#',
+    demoLink: '#',
+  },
+
+];
+
+const ProjectCards = document.querySelector('#portfolio-section');
+const ModalTitle = document.querySelector('.modaltitle');
+const ModalDescription = document.querySelector('.modaldesc');
+const ModalPhoto = document.querySelector('.modalphoto');
+const ModalSeeSource = document.querySelector('.source');
+const ModalSeeLive = document.querySelector('.live');
+const ModalTech = document.querySelector('.technologies');
+const ModalBack = document.querySelector('.modal-back')
+
+
+ProjectCards.innerHTML = '';
+
+for (let i = 0; i < projectsList.length; i++) {
+
+  let tech_lang = '';
+  projectsList[i].technologies.forEach((projTech) => {
+    tech_lang += `<li class="lang">${projTech}</li>`;
+  });
+
+  ProjectCards.innerHTML += `
+  <div class="project-cards">
+  <img class="img"
+  src="${projectsList[i].imageLink}"
+  alt="Project Featured Image">
+
+<div class="project-description">
+  <h1 class="project-title">${projectsList[i].mdltitle}</h1>
+  <ul class="project-name">
+  <li>${projectsList[i].company}</li>
+  <li class="bullet">&#x2022;</li>
+  <li>${projectsList[i].role}</li>
+  <li class="bullet">&#x2022;</li>
+  <li>${projectsList[i].year}</li>
+</ul>
+<p class="description">${projectsList[i].description}
+</p>
+
+<ul class="technologies">
+  ${tech_lang}
+</ul>
+<div>
+  <button class="button default_button" id="seeproject-${i}"
+      type="button"
+      >See Project</button>
+</div>
+</div>
+</div>
+`
+
+}
+
+
+
+// for (j = 0; j < projectsList.length; j++) {
+
+//   document.querySelectorAll(`.default_button_${j}`).addEventListener("click", function () {
+
+//     // let modaltech = '';
+//     // projectsList[i].technologies.forEach((projTech) => {
+//     //   modaltech += `<li class="lang">${projTech}</li>`;
+//     // });
+
+//   })
+
+
+// }
+
+
+for (let i = 0; projectsList.length; i++) {
+  document.querySelector(`#seeproject-${i}`).addEventListener('click', () => {
+    let modaltechnology = ''
+    projectsList[i].technologies.forEach((language) => {
+      modaltechnology += `
+        <li>${language}</li>
+      `
+    })
+
+    ModalTitle.innerHTML = projectsList[i].mdltitle
+    ModalPhoto.src = projectsList[i].imageLink
+    ModalDescription.innerHTML = projectsList[i].description
+    ModalTech.innerHTML = modaltechnology
+    ModalBack.style.display = 'block'
+  })
+}
+
+//Close Modal
+
+function CloseModal() {
+  ModalBack.style.display = 'none'
+
+}
+
+
+
+
+
+
+
+
+
+//Contact form Validation
+
 const contactForm = document.querySelector('form');
 const EmailValue = document.getElementById('email');
 const NameValue = document.getElementById('name');
 const MessageValue = document.getElementById('message');
 
-contactForm.addEventListener("input", ()=>{
-const formData = {
-name: NameValue.value,
-email: EmailValue.value,
-message: MessageValue.value,
-};
-localStorage.setItem('userData', JSON.stringify(formData));
+contactForm.addEventListener("input", () => {
+  const formData = {
+    name: NameValue.value,
+    email: EmailValue.value,
+    message: MessageValue.value,
+  };
+  localStorage.setItem('userData', JSON.stringify(formData));
 });
 
 const GetDataFromLocalStorage = JSON.parse(localStorage.getItem('userData'));
@@ -53,24 +204,23 @@ linkContact.addEventListener('click', () => {
 function validate() {
 
   var txt = document.getElementById("email").value;
-  var regx =  /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  var regx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
-  document.forms[0].onsubmit = function(e) {
-      if(regx.test(txt)) {
-          document.getElementById("msg1").innerHTML = "Valid";
-          document.getElementById("msg1").style.visibility = "visible";
-          document.getElementById("msg1").style.color = "white";
-          document.getElementById("msg1").style.background = "green";
-      }
-      else{ 
-          document.getElementById("msg1").innerHTML = "Invalid !!! please enter the email in lower case";
-          document.getElementById("msg1").style.visibility = "visible";
-          document.getElementById("msg1").style.color = "white";
-          document.getElementById("msg1").style.background = "red";
-          e.preventDefault();
-      }
+  document.forms[0].onsubmit = function (e) {
+    if (regx.test(txt)) {
+      document.getElementById("msg1").innerHTML = "Valid";
+      document.getElementById("msg1").style.visibility = "visible";
+      document.getElementById("msg1").style.color = "white";
+      document.getElementById("msg1").style.background = "green";
+    }
+    else {
+      document.getElementById("msg1").innerHTML = "Invalid !!! please enter the email in lower case";
+      document.getElementById("msg1").style.visibility = "visible";
+      document.getElementById("msg1").style.color = "white";
+      document.getElementById("msg1").style.background = "red";
+      e.preventDefault();
+    }
   }
 }
 
 
- 
